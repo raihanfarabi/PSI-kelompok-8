52b2655d8cbf29a2ed9f56d0a920550c4afbccbf
raihanfarabi <raihanfarabi2005@gmail.com>
2026-06-09 19:40:02 +0700

    Update app.js

--- isd-project/frontend/app.js
+++ isd-project/frontend/app.js
@@ -1,3 +1,20 @@
 fetch("http://localhost:3000/books")
-.then(response => response.json())
-.then(data => console.log(data));
+  .then(response => response.json())
+  .then(data => {
+
+    const container = document.getElementById("book-list");
+
+    data.forEach(book => {
+      const div = document.createElement("div");
+
+      div.className = "book";
+
+      div.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>
+      `;
+
+      container.appendChild(div);
+    });
+
+  });
