45704609cf49662e5c21fad5ebcb09a3301e819d
raihanfarabi <raihanfarabi2005@gmail.com>
2026-06-09 19:42:56 +0700

    Update server.js

--- isd-project/backend/server.js
+++ isd-project/backend/server.js
@@ -8,12 +8,30 @@ app.use(cors());
 app.use(express.json());
 
 const books = [
-  { id: 1, title: "Clean Code", author: "Robert C. Martin" },
-  { id: 2, title: "Atomic Habits", author: "James Clear" }
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert C. Martin"
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear"
+  }
 ];
 
 app.get("/books", (req, res) => {
   res.json(books);
+});
+
+app.post("/books", (req, res) => {
+  const newBook = req.body;
+  books.push(newBook);
+
+  res.json({
+    message: "Book added successfully",
+    data: newBook
+  });
 });
 
 app.listen(PORT, () => {
